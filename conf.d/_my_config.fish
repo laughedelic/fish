8b83e3c0603205dc478682a57325ae2e0c34f430
Alexey Alekhin <laughedelic@gmail.com>
2016-05-24 03:00:13 +0200

    Added a couple of useful key bindings

--- conf.d/_my_config.fish
+++ conf.d/_my_config.fish
@@ -1,7 +1,3 @@
-function fish_right_prompt
-  gtm prompt-status
-end
-
 # use j for autojump (z plugin)
 set -U Z_CMD "j"
 
@@ -10,3 +6,12 @@ set -U VISUAL "atom-beta --wait"
 set -U EDITOR $VISUAL
 
 set -U fish_color_search_match --background=cyan
+
+set -l IFS; set -U fish_greeting (fish_logo); set -e IFS
+
+# set -g fish_key_bindings fish_vi_key_bindings
+
+# set -g fish_cursor_default underscore
+# set -g fish_cursor_insert line
+# # set -g fish_cursor_replace-one underscore
+# set -g fish_cursor_visual underscore
--- key_bindings.fish
+++ key_bindings.fish
@@ -1,6 +1,10 @@
 function key_bindings
 
-  bind \cl history-search-backward
-  bind \ck history-search-forward
+    bind \cl history-search-backward
+    bind \ck history-search-forward
 
+    # resets the command line, but leaves the command (without executing it)
+    bind \cx 'echo; commandline -f repaint; commandline -r ""'
+
+    bind \cf 'accept-autosuggestion; execute'
 end
