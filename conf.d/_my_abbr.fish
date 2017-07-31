58d2ba891b7676b1edbfb6e7ff4d100b8d68b71b
Alexey Alekhin <laughedelic@gmail.com>
2017-07-31 10:50:06 +0200

    Added conditional abbreviations loading

---
+++ conf.d/_my_abbr.fish
@@ -0,0 +1,31 @@
+# it should run only once on plugin install
+if [ not (set -q _abbr_set) ]
+    echo 'Creating abbreviations...'
+
+    # general:
+    abbr fi fisher
+    abbr dev 'cd /Users/laughedelic/dev'
+    abbr a 'atom-beta'
+    abbr tr 'trash'
+    abbr rmrf 'rm -rf'
+    # aws:
+    abbr awsls 'aws s3 ls --human-readable s3://'
+    # git:
+    abbr g    'git'
+    abbr gb   'git branch'
+    abbr gbd  'git branch -d'
+    abbr ga   'git add'
+    abbr gst  'git status'
+    abbr gs   'git status -s'
+    abbr gco  'git checkout'
+    abbr gcb  'git checkout -b'
+    abbr gcom 'git checkout master'
+    abbr gc   'git commit'
+    abbr gcm  'git commit -m'
+    abbr gca  'git commit -am'
+    abbr gd   'git diff'
+    abbr gp   'git push'
+    abbr gl   'git pull'
+
+    set -U _abbr_set true
+end
---
+++ uninstall.fish
@@ -0,0 +1,1 @@
+set -e _abbr_set
