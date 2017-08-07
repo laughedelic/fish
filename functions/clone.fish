d331bd6e58e204ce50de28641a32ad2451be3dbc
Alexey Alekhin <laughedelic@gmail.com>
2017-08-07 13:26:04 +0200

    Added fork function; improved clone

--- functions/clone.fish
+++ functions/clone.fish
@@ -4,27 +4,32 @@ function clone --argument-names arg prefix --description 'Clones a repository to
     or set -U clone_prefix ~/dev
 
     function print_usage
+        echo 'Clones a repository to <prefix>/<org>/<name> and jumps there'
         echo
         echo 'Usage: clone [https://github.com/]<org>/<name>[.git] [<prefix>]'
+        echo
         echo "Default <prefix> is stored in the universal var \$clone_prefix: $clone_prefix"
     end
 
-    if test (count $argv) = 0
-        print_usage
-        return 1
-    end
+    test (count $argv) = 0
+    or [ $argv[1] = "-h" ]
+    or [ $argv[1] = "--help" ]
+    and print_usage; and return 0
 
-    set org (basename (dirname "$arg"))
-    set name (basename "$arg" .git)
-    set repo "$org/$name"
+    set -l org (basename (dirname "$arg"))
+    set -l name (basename "$arg" .git)
+    set -l repo "$org/$name"
 
     test -z "$prefix"
-    and set prefix $clone_prefix
+    and set -l prefix $clone_prefix
 
     test -d $prefix/$repo
     and echo "$repo already exists"
     or git clone https://github.com/$repo.git $prefix/$repo
 
     cd $prefix/$repo
-    or print_usage
+    or begin
+        print_usage
+        return 1
+    end
 end
---
+++ functions/fork.fish
@@ -0,0 +1,50 @@
+function fork --argument-names arg prefix --description 'Clones a repository to <prefix>/<user>/<name> and jumps there'
+
+    set -l user (git config --global github.user; or whoami)
+    or begin
+        echo 'Error: github.user is unset!'
+        print_usage
+        return 1
+    end
+
+    function print_usage
+        echo 'Clones a repository to <prefix>/<user>/<name>, jumps there and sets up the remotes:'
+        echo '* `upstream` corresponds to the repository you fork from'
+        echo '* `origin`   corresponds to your fork'
+        echo
+        echo 'Usage: fork [https://github.com/]<org>/<name>[.git] [<prefix>]'
+        echo
+        echo "Default <prefix> is stored in the universal var \$clone_prefix: $clone_prefix"
+        echo "Variable <user> is set from the global github.user setting: "(git config --global github.user)
+    end
+
+    test (count $argv) = 0
+    or [ $argv[1] = "-h" ]
+    or [ $argv[1] = "--help" ]
+    and print_usage; and return 0
+
+    set -l org (basename (dirname "$arg"))
+    set -l name (basename "$arg" .git)
+    set -l fork "$user/$name"
+
+    test -z "$prefix"
+    and set -l prefix $clone_prefix
+
+    test -d $prefix/$fork
+    and echo "$fork already exists"
+    or git clone https://github.com/$org/$name.git $prefix/$fork
+
+    cd $prefix/$fork
+    or begin
+        print_usage
+        return 1
+    end
+
+    git remote rename origin upstream
+    git remote add origin https://github.com/$fork.git
+
+    echo
+    echo 'git remotes:'
+    echo '* upstream: '(git remote get-url upstream)
+    echo '* origin:   '(git remote get-url origin)
+end
