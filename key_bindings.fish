8ebef31b72a3477ecb43a18e929a9a307c308a2d
Alexey Alekhin <laughedelic@gmail.com>
2018-01-14 19:12:59 +0100

    Fixing keybindings

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,10 +1,12 @@
 function key_bindings
 
-    bind \cl history-search-backward
-    bind \ck history-search-forward
+    bind -M insert \cp history-search-backward
+    bind -M insert \cn history-search-forward
+    bind -M default \cp history-search-backward
+    bind -M default \cn history-search-forward
 
     # resets the command line, but leaves the command (without executing it)
-    bind \cx 'echo; commandline -f repaint; commandline -r ""'
+    bind -M insert \cx 'echo; commandline -f repaint; commandline -r ""'
 
-    bind \cf 'commandline -f accept-autosuggestion execute'
+    bind -M insert \cf 'commandline -f accept-autosuggestion execute'
 end
